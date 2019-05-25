fd52b8183037bedc22d182779947ff654b3446e6
Connor Cahill <connorcahill913@gmail.com>
2019-05-25 15:36:11 -0700

    [Adds]: Admin sectins to user schema

--- src/user.model.js
+++ src/user.model.js
@@ -7,6 +7,7 @@ const UserSchema = new Schema({
   email: { type: String, required: true },
   phoneNumber: { type: String, required: true },
   password: { type: String, required: true },
+  admin: { type: Bool, default: false },
 });
 
 
